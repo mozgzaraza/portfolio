c477b0c9577205f3fd0632fcaadb71a3649d20a9
mozgzaraza <sattar.isya@gmail.com>
2023-09-06 15:26:36 +0300

    Добавил модальное окно с ответом от сервера

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,40 @@
+// ПОПАП
 const popupLinks = document.querySelectorAll(".popup-link");
 const popup = document.querySelector(".popup");
 const popupClose = document.querySelector(".popup__close");
 
-function openClose() {
+function openClosePopup() {
   popup.classList.toggle("open");
 }
 
 popupLinks.forEach(function (value, key) {
-  value.addEventListener("click", openClose);
+  value.addEventListener("click", openClosePopup);
 });
 
-popupClose.addEventListener("click", openClose);
+popupClose.addEventListener("click", openClosePopup);
+
+// ВЫЗОВ ОТВЕТА
+const answerLink = document.querySelector(".popup__button");
+const answerPopup = document.querySelector(".popup-answer");
+const answerClose = document.querySelector(".popup-answer__close");
+const answerMessage = function (message) {
+  document.querySelector(".popup-answer__text").textContent = message;
+};
+let answerNum = 0;
+
+function openCloseAnswer() {
+  answerPopup.classList.toggle("open");
+}
+
+answerLink.addEventListener("click", openCloseAnswer);
+
+answerClose.addEventListener("click", function () {
+  if (answerNum === 1) {
+    openCloseAnswer();
+    openClosePopup();
+    answerMessage("Все ОК");
+  } else {
+    openCloseAnswer();
+    answerMessage("Все не ОК, попробуй еще раз");
+  }
+});
